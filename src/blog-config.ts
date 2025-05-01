5e3d9aa27e6e65b8f4ae22a6e89d702939d6b39f
nurooong <iam@nur.ng>
2025-05-02 00:22:40 +0900

    feat: Add default site description to config

--- src/blog-config.ts
+++ src/blog-config.ts
@@ -2,6 +2,7 @@ import { encodeNumberToBase62 } from "@/lib/base62";
 
 export const GENERAL_CONFIG = {
   siteTitle: 'nurooong',
+  description: 'nurooong\'s carefully unplanned blog',
 }
 
 export const ROUTING_CONFIG = {
